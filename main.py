c2059040150f512c5d0f346627493d33eaba030f
AlanMorrisWright <53399739+AlanMorrisWright@users.noreply.github.com>
2021-12-19 17:45:36 +0000

    moved market.json parser to separate file

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-def get_prices():
-    with open('C:\\!\\CODING\\ED\\J\\Market.json') as f:
-        lines = f.readlines()
-
-    station = {}
-    prices = []
-    i = 0
-    for line in lines:
-        if line:
-            if i == 0:
-                neat_line = line\
-                    .replace('{ ', '')\
-                    .replace(', "Items":[ ', '')\
-                    .replace(chr(10), '')\
-                    .replace('":', '"|')\
-                    .replace('"', '')
-                station = dict((k.strip(), v.strip()) for k, v in (item.split('|') for item in neat_line.split(',')))
-                i = i + 1
-            else:
-                neat_line = line\
-                    .replace('{ ', '') \
-                    .replace(' },', '') \
-                    .replace(' }', '') \
-                    .replace(chr(10), '') \
-                    .replace('":', '"|') \
-                    .replace('"', '')
-                if neat_line[:3] == 'id|':
-                    item_prices = dict((k.strip(), v.strip()) for k, v in (item.split('|') for item in neat_line.split(',')))
-                    prices.append(item_prices)
-    return station, prices
+from market import get_prices
 
 
 def main():
-    station, prices = get_prices()
+    station, prices = get_prices('C:\\!\\CODING\\ED\\J\\', 'Market.json')
     for f in ['timestamp', 'StationName', 'StarSystem']:
         print(f'{f}: {station[f]}')
     for item_price in prices:
