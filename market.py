22e97e6a91304aadba082c9c754db32e06b88c7a
AlanMorrisWright <53399739+AlanMorrisWright@users.noreply.github.com>
2021-12-19 17:56:44 +0000

    pep8 tweaks

--- market.py
+++ market.py
@@ -18,13 +18,15 @@ def get_prices(file_path, file_name):
                 i = i + 1
             else:
                 neat_line = line\
-                    .replace('{ ', '') \
-                    .replace(' },', '') \
-                    .replace(' }', '') \
-                    .replace(chr(10), '') \
-                    .replace('":', '"|') \
+                    .replace('{ ', '')\
+                    .replace(' },', '')\
+                    .replace(' }', '')\
+                    .replace(chr(10), '')\
+                    .replace('":', '"|')\
                     .replace('"', '')
                 if neat_line[:3] == 'id|':
-                    item_prices = dict((k.strip(), v.strip()) for k, v in (item.split('|') for item in neat_line.split(',')))
+                    item_prices = dict(
+                        (k.strip(), v.strip()) for k, v in (item.split('|') for item in neat_line.split(','))
+                    )
                     prices.append(item_prices)
     return station, prices
